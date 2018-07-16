52dc4d55587acb1194ef6922e5cf1d4e9e714810
Helen Dechat <hdechat@gmail.com>
2018-07-16 14:14:41 -0600

    Add seed data to dev file

--- db/seeds/dev/stuff.js
+++ db/seeds/dev/stuff.js
@@ -1,7 +1,11 @@
 
 exports.seed = (knex, Promise) => {
   return knex('items').del()
-    .then(() => {
-      return knex('items').insert(items);
+    .then( () => {
+      return Promise.all([
+        knex('items').insert({item: 'helmet', packed: false}),
+        knex('items').insert({item: 'space suite', packed: false}),
+        knex('items').insert({item: 'freeze dried icecream', packed: false}),
+      ]);
     });
 };

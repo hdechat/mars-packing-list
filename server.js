db865c285e9d33ac68634c9d4cc95aade9fb2494
Helen Dechat <hdechat@gmail.com>
2018-07-16 10:56:47 -0600

    Add initial server file structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on ${app.get('port')}.`);
+});
